da42b6e1712d2d902ce6e64920576e6afedb125b
Daan Vanden Bosch <daan.v.d.bosch@gmail.com>
2024-07-25 17:54:42 +0200

    Added paging.

--- dev_server.js
+++ dev_server.js
@@ -1,15 +1,17 @@
-import { serve } from 'https://deno.land/std@0.140.0/http/server.ts';
 import { serveDir } from 'https://deno.land/std@0.140.0/http/file_server.ts';
 
-serve(async req => {
-    if (new URL(req.url).pathname === '/') {
-        return new Response(
-            await Deno.readFile("src/index.html"),
-            {
-                headers: { 'content-type': 'text/html' }
-            },
-        );
-    }
+Deno.serve(
+    { hostname: '0.0.0.0' },
+    async req => {
+        if (new URL(req.url).pathname === '/') {
+            return new Response(
+                await Deno.readFile("src/index.html"),
+                {
+                    headers: { 'content-type': 'text/html' }
+                },
+            );
+        }
 
-    return serveDir(req, { fsRoot: 'src' });
-});
+        return serveDir(req, { fsRoot: 'src' });
+    },
+);
